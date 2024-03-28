2aae0302a495102048e3257fedf4392351f84219
Assanali18 <ustaevasanali@gmail.com>
2024-03-29 02:08:28 +0600

    update gitignore

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiKey: "AIzaSyBxZalNwgvbM09GWMFquKj87jSb8_cuHw0",
-    fbDbUrl: "https://angular-shop-74a78-default-rtdb.firebaseio.com/"
-
-};
